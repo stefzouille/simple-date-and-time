7c88258fd7eef3cb76ae910cbf9c1100d727f984
stefzouille <contact.lokcy@gmail.com>
2021-05-05 10:04:22 +0200

    add

--- src/App.js
+++ src/App.js
@@ -11,18 +11,19 @@ function App() {
   return (
     <div className="App mt-50rem text-center">
       <header >
-        <div className="py-5">
-       <h1>JUST A SIMPLE DATE AND HOUR :</h1> 
-        <p>By c0c0nut 0x9</p>
-       </div>
-     <section className="text-danger fs-1"><hr/>
-      <div className="mb-50rem mb-50rem">
-        </div>
-        <div  className="mt-50rem ">
-      <Time />
-      </div>
-       </section> 
-       <hr/>
+            <div className="py-5">
+              <h1>JUST A SIMPLE DATE AND HOUR :</h1> 
+              <p>By c0c0nut 0x9</p>
+              <p className="text-success fs-3 mt-30rem">Have a nice day !</p>
+          </div>
+        <section className="text-danger fs-1">
+            <hr/>
+          <div className="mb-50rem mb-50rem">
+            <p className="text-secondary">today we are :</p>
+              <Time />
+          </div>
+        </section> 
+          <hr/>
       </header>
        
       
--- src/components/Time.js
+++ src/components/Time.js
@@ -1,25 +1,23 @@
 import React from "react";
 
 export default class App extends React.Component {
-  state = {
-    date: "" , 
-    
-   
-  };
- componentDidMount() {
-    this.getDate();
+    state = {
+      date: "" , 
+      };
+  componentDidMount() {
+      this.getDate();
+      }
+      
+    getDate = () => {
+      var date = new Date().toLocaleString()
+      this.setState({ date });
+      window.setInterval(() => this.getDate(), 1000)
+    };
+    render() {
+      const { date } = this.state;
+        return (
+        <div >{date}
+        </div>
+        )
     }
-     //date ok mais heure non mis a jour a mettre dans un useEffect?
-  getDate = () => {
-    var date = new Date().toLocaleString()
-    this.setState({ date });
-    window.setInterval(() => this.getDate(), 1000)
-  };
-  render() {
-    const { date } = this.state;
-      return (
-      <div >{date}
-      </div>
-      )
-  }
   }
