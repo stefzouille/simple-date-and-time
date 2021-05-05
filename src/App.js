0e1b77d4e0c250be455efc8cb9072dc981037b2d
stefzouille <contact.lokcy@gmail.com>
2021-05-05 09:48:42 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Time from './components/Time'
-  import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+  // import './App.css';
 
 
 
@@ -8,15 +9,20 @@ import Time from './components/Time'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App mt-50rem text-center">
       <header >
-        <div >
+        <div className="py-5">
        <h1>JUST A SIMPLE DATE AND HOUR :</h1> 
+        <p>By c0c0nut 0x9</p>
        </div>
-      <div >By c0c0nut 0x9</div>
-     <section >
+     <section className="text-danger fs-1"><hr/>
+      <div className="mb-50rem mb-50rem">
+        </div>
+        <div  className="mt-50rem ">
       <Time />
+      </div>
        </section> 
+       <hr/>
       </header>
        
       
