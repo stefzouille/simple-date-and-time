6ff49a4678999688ed7d5659bee4cdce5a9f5339
stefzouille <contact.lokcy@gmail.com>
2021-05-05 09:24:25 +0200

    add

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Time from './components/Time'
 
 function App() {
   return (
-    <div className="App p-3 text-center bg-dark text-white">
+    <div className="App p-3 ">
       <header>
         <div>
        <h1>JUST A SIMPLE DATE AND HOUR :</h1> 
        </div>
       </header>
-      <div className="">By c0c0nut 0x9</div>
-     <section className="">
+      <div >By c0c0nut 0x9</div>
+     <section >
        
       <Time />
        </section> 
